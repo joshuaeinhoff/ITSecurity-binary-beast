99f037b5d794e580afb153640510a643950bb703
Joshi <joshualeinhoff@gmail.com>
2024-07-07 21:48:55 +0200

    added pictures for cbc explanation and tabs to switch between modes

--- XorVisualization.py
+++ XorVisualization.py
@@ -1,8 +1,11 @@
 import tkinter as tk
+from tkinter import ttk as ttk
 import time
 
 window = tk.Tk()
 photo = tk.PhotoImage(file = 'assets/xorIcon.png')
+encryptGraphic = tk.PhotoImage(file='assets\CBC_Encrypt.png')
+decryptGraphic = tk.PhotoImage(file='assets\CBC_Decrypt.png')
 window.wm_iconphoto(False, photo)
 # width= window.winfo_screenwidth() 
 # height= window.winfo_screenheight()
@@ -12,6 +15,14 @@ font = 'JetBrains Mono'
 fontSize = 16
 
 xorString = '' 
+
+tabControl = ttk.Notebook(window) 
+tab1 = ttk.Frame(tabControl) 
+tab2 = ttk.Frame(tabControl) 
+
+tabControl.add(tab1, text ='Info') 
+tabControl.add(tab2, text ='XorVisualization') 
+tabControl.pack(expand = 1, fill ="both") 
 
 
 def binaryTransform(word):
@@ -79,11 +90,11 @@ def animateXor(word1,word2):
                 highlightCharacter(labelLine1, word2, i)
                 result += showCalculation(word1[i], word2[i],outputCalculation)
                 showResult(outputXor,result)
-                window.update()
+                tab2.update()
                 time.sleep(0.500)
         displayNormal(labelLine0, word1)
         displayNormal(labelLine1, word2)
-        window.update()
+        tab2.update()
     return result
 
 def showCalculation(a, b, label):
@@ -107,61 +118,66 @@ def recoverPlain():
 # -------- Input Section --------
 input_variable1 = tk.StringVar()
 input_variable2 = tk.StringVar()
-tk.Label(window, text='Plaintext').grid(column=0, row=0, pady='5', padx='5')
-tk.Label(window, text='Initialization Vector').grid(column=0, row=1, pady='5', padx='5')
-input1 = tk.Entry(window, textvariable=input_variable1)
-input2 = tk.Entry(window, textvariable=input_variable2)
+tk.Label(tab2, text='Plaintext').grid(column=0, row=0, pady='5', padx='5')
+tk.Label(tab2, text='Initialization Vector').grid(column=0, row=1, pady='5', padx='5')
+input1 = tk.Entry(tab2, textvariable=input_variable1)
+input2 = tk.Entry(tab2, textvariable=input_variable2)
 input1.grid(column=1, row=0,padx=10)
 input2.grid(column=1, row=1, padx=10)
 # -------- Input Section --------
 
 
 # -------- Button Section --------
-confirmButton = tk.Button(window, 
+confirmButton = tk.Button(tab2, 
                         text = "Confirm",  
                         command = confirmInput) 
 confirmButton.grid(column=5, row=5)
 
-reXorButton = tk.Button(window,
+reXorButton = tk.Button(tab2,
                         text="Recover Plaintext",
                         command= recoverPlain)
 reXorButton.grid(column=5, row=6)
 # -------- Button Section --------
 
 # -------- Plaintext label --------
-plaintext = tk.Label(window, font=(font,12))
+plaintext = tk.Label(tab2, font=(font,12))
 plaintext.grid(column=5,row=0)
 
 # -------- Labelline 1 --------
-outputLabel0 = tk.Label(window, padx=0,font=(font,fontSize))
+outputLabel0 = tk.Label(tab2, padx=0,font=(font,fontSize))
 outputLabel0.grid(column=2, row=2)
-outputLabel1 = tk.Label(window, padx=0, font=(font,fontSize))
+outputLabel1 = tk.Label(tab2, padx=0, font=(font,fontSize))
 outputLabel1.grid(column=3, row=2)
-outputLabel2 = tk.Label(window, padx=0, font=(font,fontSize))
+outputLabel2 = tk.Label(tab2, padx=0, font=(font,fontSize))
 outputLabel2.grid(column=4, row=2)
 labelLine0 = [outputLabel0,outputLabel1,outputLabel2]
 # -------- Labelline 1 --------
 
 
 # -------- Labelline 2 --------
-outputLabel3 = tk.Label(window, padx=0, font=(font,fontSize))
+outputLabel3 = tk.Label(tab2, padx=0, font=(font,fontSize))
 outputLabel3.grid(column=2, row=3)
-outputLabel4 = tk.Label(window, padx=0, font=(font,fontSize))
+outputLabel4 = tk.Label(tab2, padx=0, font=(font,fontSize))
 outputLabel4.grid(column=3, row=3)
-outputLabel5 = tk.Label(window, padx=0, font=(font,fontSize))
+outputLabel5 = tk.Label(tab2, padx=0, font=(font,fontSize))
 outputLabel5.grid(column=4, row=3)
 labelLine1 = [outputLabel3,outputLabel4,outputLabel5]
 # -------- Labelline 2 --------
 
 # -------- Calculation --------
-outputCalculation = tk.Label(window, font=(font, fontSize))
+outputCalculation = tk.Label(tab2, font=(font, fontSize))
 outputCalculation.grid(column=2, row=4)
 # -------- Calculation --------
 
 # -------- Result --------
-outputXor = tk.Label(window, font=(font,fontSize))
+outputXor = tk.Label(tab2, font=(font,fontSize))
 outputXor.grid(column=2, row=5)
 # -------- Result --------
 
+
+image1 = tk.Label(tab1,image=encryptGraphic)
+image2 = tk.Label(tab1,image=decryptGraphic)
+image1.grid(column=0, row=0)
+image2.grid(column=0, row=1)
 
 window.mainloop()
